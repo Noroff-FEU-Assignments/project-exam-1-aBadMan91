c7bc3b1c0f26485e3929168a346310467eb21463
aBadMan91 <114421846+aBadMan91@users.noreply.github.com>
2023-05-23 22:07:05 +0200

    Version 0.9
    - Added a modal to "postspecific.html"

--- js/postspecific.js
+++ js/postspecific.js
@@ -8,12 +8,6 @@ const name = params.get("name");
 
 const url = "https://techstuff.aleksnord.no/wp-json/wp/v2/posts/" + id;
 
-// const urlMedia = "https://techstuff.aleksnord.no/wp-json/wp/v2/media";
-
-// const urlComments = "https://techstuff.aleksnord.no/wp-json/wp/v2/comments";
-
-// const urlAuthor = "https://techstuff.aleksnord.no/wp-json/wp/v2/users/1";
-
 async function getPost() {
 
     try {
@@ -37,10 +31,52 @@ document.title = "AleksTechStuff" + " " + "|" + " " + name;
 getPost();
 
 function createHtml(post) {
-    detailContainer.innerHTML = `
-            <h1>${post.title.rendered}</h1>
-        <div class="post-content">
-            ${post.content.rendered}
-        </div>
-    `;
-}
+  
+    const heading = document.createElement("h1");
+    heading.textContent = post.title.rendered;
+  
+    const postContent = document.createElement("div");
+    postContent.classList.add("post-content");
+    postContent.innerHTML = post.content.rendered;
+  
+    postContent.addEventListener("click", openModal);
+  
+    detailContainer.appendChild(heading);
+    detailContainer.appendChild(postContent);
+  }
+
+  function openModal(event) {
+    if (event.target.tagName === "IMG") {
+      const imageUrl = event.target.src;
+  
+      const modalContainer = document.createElement("div");
+      modalContainer.classList.add("modal-container");
+  
+      const modalContent = document.createElement("div");
+      modalContent.classList.add("modal-content");
+  
+      const closeButton = document.createElement("span");
+      closeButton.classList.add("modal-close");
+      closeButton.innerHTML = "&times;";
+  
+      const modalImage = document.createElement("img");
+      modalImage.src = imageUrl;
+      modalImage.alt = "Modal Image";
+  
+      modalContent.appendChild(modalImage);
+      modalContent.appendChild(closeButton);
+  
+      modalContainer.appendChild(modalContent);
+  
+      document.body.appendChild(modalContainer);
+  
+      modalContainer.addEventListener("click", closeModal);
+      closeButton.addEventListener("click", closeModal);
+    }
+  }
+  
+  function closeModal() {
+    const modalContainer = document.querySelector(".modal-container");
+    modalContainer.remove();
+  }
+  
